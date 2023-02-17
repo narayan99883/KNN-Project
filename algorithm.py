f3322221870ab219c41738475d479c4df695329a
narayan99883 <63159380+narayan99883@users.noreply.github.com>
2023-02-17 12:06:54 -0500

    2/17 - need to convert strings

--- algorithm.py
+++ algorithm.py
@@ -1,7 +1,4 @@
-#Data Set
-#https://www.kaggle.com/datasets/kamilpytlak/personal-key-indicators-of-heart-disease 
-
-
+#imports
 import numpy as np
 import csv
 import matplotlib as plt
@@ -17,27 +14,51 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_validate
 from sklearn.metrics import classification_report, confusion_matrix
-bmiFullArray=[]
-bmiArray=[]
-bmiPositive=[]
-bmiNegative=[]
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
+from collections import defaultdict
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+
+
+#Read in dataset using pandas
 filename='heart_2020_cleaned.csv'
-with open(filename) as f:
-    reader = csv.reader(f)
-    header_row = next(reader)
-    print(header_row)
-    for row in reader:
-        bmiFullArray.append(row[1])
-    
+numberOfRows = 100
+dSet = pd.read_csv(filename, nrows=numberOfRows)
 
-            
-        
+#print(dSet.to_string())
 
-#plt.figure(figsize=(8,6))
-#plt.scatter( bmiArray,bmiArray, s=50, color='r')
-#plt.xlabel('BMI')
+#Turning Strings into ints with dictionary attached
+le = preprocessing.LabelEncoder()
+OneHotEncoder().fit_transform(dSet)
+#Dictionary
+d = defaultdict(LabelEncoder)
 
-#plt.title('scatter plot R Value: '+str(R_sq))
-#plt.plot(x,a*x+b)
-#plt.show()
-    
+print(dSet.to_string())
+
+#Splitting dataset by target and feature
+dSet_target = dSet[["HeartDisease"]]
+print(dSet_target.to_string())
+dSet_features = dSet[["BMI", "Smoking", "Stroke", "Sex", "AgeCategory", "Race", "Diabetic", "PhysicalActivity", "SleepTime", "Asthma"]]
+print(dSet_features.to_string())
+
+#Getting data ready for testing
+X_train, X_test, y_train, y_test = train_test_split(dSet_features, dSet_target, test_size=0.3)
+
+
+#normalize data
+scaler = StandardScaler()
+scaler.fit(X_train)
+StandardScaler()
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+classifier = KNeighborsClassifier(n_neighbors=10)
+classifier.fit(X_train, np.ravel(y_train,order='C'))
+KNeighborsClassifier(n_neighbors=10)
+y_pred = classifier.predict(X_test)
+
+#Printing results
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
